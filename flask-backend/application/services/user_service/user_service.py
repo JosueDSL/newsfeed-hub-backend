112a4aae19f9bebba713d671e701da9fbf6953a1
JosueDSL <josuedesantosl@gmail.com>
2024-09-08 00:06:30 -0600

    Add services structure and user class for handling user related operations

---
+++ flask-backend/application/services/user_service/user_service.py
@@ -0,0 +1,15 @@
+# Description: User service class to handle user related operations.
+
+
+class UserService:
+    """
+    User service class to handle user related operations.
+
+    """
+
+    pass
+    # def __init__(self, user_repository):
+    #     self.user_repository = user_repository
+
+    # def get_user(self, user_id):
+    #     return self.user_repository.get_user(user_id)
