7450234471aac761fbd85e8e48e1e22e340198ef
JosueDSL <josuedesantosl@gmail.com>
2024-09-09 13:44:08 -0600

    Add ResourcesService class for managing resources in the database

---
+++ flask-backend/application/services/feeds_service/resources_service.py
@@ -0,0 +1,11 @@
+# Description: Service layer for resources
+
+from application.models import Resource
+
+
+class ResourcesService:
+    """
+    A service class to interact with the resources in the database.
+    """
+
+    pass
